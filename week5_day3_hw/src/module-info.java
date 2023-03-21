f965e4edd737f81e041ab7a0dd24224aa2694e63
zoe6298 <wwsong0421@gmail.com>
2023-03-20 21:18:53 -0500

    first commit

---
+++ week5_day3_hw/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author chenli
+ *
+ */
+module week5_day3_hw {
+}
---
+++ week5_day3_hw/src/week5_day3_hw/CompilerError.java
@@ -0,0 +1,19 @@
+package week5_day3_hw;
+
+public class CompilerError {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		try {
+			String illegal-names = "abc";
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		int uninitializedItem;
+
+	}
+
+}
